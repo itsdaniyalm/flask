ade0e0ab961b6476d4b44ac652f0c27f958d87cd
Daniyal M <itsdaniyalm@gmail.com>
2022-11-10 16:42:49 +0500

    Integerated database and forms

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf.csrf import CSRFProtect
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -13,6 +15,9 @@ Bootstrap(app) # for enabling bootstrapt templates
 Moment(app) # for date time conversion operations
 CSRFProtect(app) # for csrd protection in flast wtf froms
 app.config['SECRET_KEY'] = 'running flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5433/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -34,16 +39,42 @@ def internal_server_error(e):
 def form():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('form'))
-    return render_template('form.html', form=form, name=session.get('name'))
+    return render_template('form.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 if __name__ == '__main__':
     app.run(debug=True)
